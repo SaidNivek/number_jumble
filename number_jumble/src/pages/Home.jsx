6f829df776a249469f205eee86da63562a9261e5
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-19 16:01:45 -0500

    Button going to GameScreen page working

--- number_jumble/src/pages/Home.jsx
+++ number_jumble/src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom'
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div>
+        <div>
+            <Link to='/gamescreen'><button className="btn btn-primary">Start Game</button></Link>
+        </div>
+    </div>
+    </>
   )
 }
