7d6680311c631e8d2c621ae276ff55873df1d27c
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-20 15:45:50 -0500

    Add icons to function buttons

--- number_jumble/src/components/Functions.jsx
+++ number_jumble/src/components/Functions.jsx
@@ -5,7 +5,7 @@ import SplitFunction from './functions/SplitFunction'
 
 function Functions() {
   return (
-    <div className='d-flex justify-content-evenly mt-3'>
+    <div className='d-flex justify-content-evenly mt-3 gap-2 col-4'>
         <AddFunction />
         <SplitFunction />
         <SubtractFunction />
