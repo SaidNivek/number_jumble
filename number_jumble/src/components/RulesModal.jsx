f35f6d4e2761ab83a0a5bc9471556582e0a621a5
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-21 10:38:34 -0500

    Begin building RulesModal

---
+++ number_jumble/src/components/RulesModal.jsx
@@ -0,0 +1,5 @@
+export default function RulesModal() {
+  return (
+    <div>RulesModal</div>
+  )
+}
--- number_jumble/src/pages/Home.jsx
+++ number_jumble/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
+import RulesModal from '../components/RulesModal'
 
 export default function Home() {
   return (
     <>
     <div>
-        <div>
+        <div className='d-flex justify-content-evenly'>
             <Link to='/gamescreen'><button className="btn btn-primary">Start Game</button></Link>
+            <RulesModal />
         </div>
     </div>
     </>
