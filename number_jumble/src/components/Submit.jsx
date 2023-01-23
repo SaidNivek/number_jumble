5b7940de65c62c4249b3dc741b5e31cc2fe9b9f6
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-23 17:29:50 -0500

    Add Home button on gamescreen that connects to homescreen

--- number_jumble/src/components/Submit.jsx
+++ number_jumble/src/components/Submit.jsx
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card'
 
 function Submit() {
   return (
-    <Card style={{border:'2px grey solid', backgroundColor:'lightblue'}} className='mt-3 col-3'>
+    <Card style={{border:'2px grey solid', backgroundColor:'lightblue'}} className='mt-3 mb-3'>
         <Card.Body className='d-flex justify-content-center'>Submit</Card.Body>
     </Card>
   )
--- number_jumble/src/pages/GameScreen.jsx
+++ number_jumble/src/pages/GameScreen.jsx
@@ -4,6 +4,7 @@ import CurrentMath from '../components/CurrentMath'
 import PreviousMath from '../components/PreviousMath'
 import GameInfo from '../components/GameInfo'
 import Submit from '../components/Submit'
+import { Link } from 'react-router-dom'
 
 function GameScreen() {
 
@@ -13,7 +14,11 @@ function GameScreen() {
       <Functions />
       <Numbers />
       <CurrentMath />
-      <Submit />
+      <div className="d-flex flex-column justify-content-center w-25">
+        <Submit />
+        <hr />
+        <Link to='/'><button className="btn btn-secondary mt-3 link w-100">Home</button></Link>
+      </div>
       <p>When a unique correct answer is achieved, the game will play a jingle and/or animation, then add the variation to the # correct tracker, then reset.</p>
       <p>Do we want to have a maximum # correct that will display to the user when they've reached them ALL or allow them to go forever until the time runs out or they hit 'Submit'?</p>
       <p>It seems strange that the player could get all the answers and NOT be told they've got the correct answers.</p>
