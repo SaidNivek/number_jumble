4e607eb4317275f8d65873c90084c50e8e2f2dfa
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-24 17:56:26 -0500

    Small changes to remove warnings

--- number_jumble/src/pages/GameScreen.jsx
+++ number_jumble/src/pages/GameScreen.jsx
@@ -17,7 +17,7 @@ function GameScreen() {
       <div className="d-flex flex-column justify-content-center w-25">
         <Submit />
         <hr />
-        <Link to='/'><button className="btn btn-secondary mt-3 link w-100">Home</button></Link>
+        <Link to='/'><button className="btn btn-secondary  link w-100">Home</button></Link>
       </div>
       <p>When a unique correct answer is achieved, the game will play a jingle and/or animation, then add the variation to the # correct tracker, then reset.</p>
       <p>Do we want to have a maximum # correct that will display to the user when they've reached them ALL or allow them to go forever until the time runs out or they hit 'Submit'?</p>
