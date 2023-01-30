e1517daa06a4c4205245b4c8a255e0967e74b749
SaidNivek <kevin.vi.dias@gmail.com>
2023-01-30 18:35:27 -0500

    Change size of function buttons

--- number_jumble/src/components/functions/SubtractFunction.jsx
+++ number_jumble/src/components/functions/SubtractFunction.jsx
@@ -5,7 +5,7 @@ import { FaMinus } from 'react-icons/fa'
 
 function SubtractFunction() {
   return (
-    <Card style={{width:'6rem', border:'2px lightgrey solid'}}>
+    <Card style={{minWidth:'3rem', border:'2px lightgrey solid'}}>
         <Card.Body className='d-flex justify-content-center align-items-center'><FaMinus /></Card.Body>
     </Card>
   )
